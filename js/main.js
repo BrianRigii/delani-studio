a3e807c89b917156b159580d5e3a38597c841229
John Brian <johnbryanjb011@gmail.com>
2019-11-04 12:21:51 +0300

    hide design image

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var design =document.getElementById("designtxt")
+var dev=document.getElementById("devtxt")
+var prod=document.getElementById("prodtxt")
+
+$("#designimg").click(function(){
+    $(this).
+    $("#designtxt").removeClass("designtxt")
+
+
+    
+
+})
+
+
+
+
+
